feebdf60d0492020c9fbf8bcf49a2b7bb2d7c3ef
Abdulqadir Abuharrus <aabuharrus@gmail.com>
2022-11-17 10:50:11 -0800

    -Clean & maintain. -Removed temp.js

--- views/temps.js
+++ views/temps.js
@@ -1,4 +1,4 @@
-
-let homeStartingContent = "This is the journal of a humble 4 that can cook, which in this economy makes me an 8, but it doesn't matter because I'm still lonely.";
-let aboutContent = "I'm just a guy trying to find himself in life, struggling through but somehow  managing to survive in this cold world.I find that writing about life, the things dealing with, and feeling helps.  It relieves some of the pressure that builds up from having no one to talk to.  It quite literally keeps me alive...";
-let contactContent = "Hey there, if you'd like to contact me, feel free to send me an email at ...";
+//
+// let homeStartingContent = "This is the journal of a humble 4 that can cook, which in this economy makes me an 8, but it doesn't matter because I'm still lonely.";
+// let aboutContent = "I'm just a guy trying to find himself in life, struggling through but somehow  managing to survive in this cold world.I find that writing about life, the things dealing with, and feeling helps.  It relieves some of the pressure that builds up from having no one to talk to.  It quite literally keeps me alive...";
+// let contactContent = "Hey there, if you'd like to contact me, feel free to send me an email at ...";
