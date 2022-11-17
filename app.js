8e6f0ed870abd3d55ff7b9806399c2abc4abb6ea
Abdulqadir Abuharrus <aabuharrus@gmail.com>
2022-11-17 11:57:55 -0800

    -Update contact.ejs -Email contact form not functional

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const _ = require("lodash");
 /// Consts & Var declarations
 const homeStartingContent = "A journal of a Lonely 4 that can cook.";
 const aboutContent = "I'm just a guy trying to find himself in life, struggling through but somehow managing to survive in this cold world.I find that writing about life, the things I'm dealing with and feeling, helps cope with them. It relieves some of the pressure that builds up from having no one to talk to. It quite literally keeps me alive...";
-const contactContent = "Hey there, if you'd like to contact me, feel free to send me an email at ...";
+const contactContent = "Hey there, if you'd like to contact me, feel free to send me an email";
 //array to store user posts
 let posts = [];
 
