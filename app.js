251bbdb2c8380cd0c8022e54608a794706381760
Abdulqadir Abuharrus <aabuharrus@gmail.com>
2022-11-23 10:15:54 -0800

    -Implement MongoDB and Mongoose. -Save entries into database. -Adjust get and post methods in app.js. -Adjust home.ejs to new changes.

--- app.js
+++ app.js
@@ -2,18 +2,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
-// const temps = require(__dirname + "temps");
-
-/// Consts & Var declarations
-const homeStartingContent = "A journal of a Lonely 4 that can cook.";
-const aboutContent = "I'm just a guy trying to find himself in life, struggling through but somehow managing to survive in this cold world.I find that writing about life, the things I'm dealing with and feeling, helps cope with them. It relieves some of the pressure that builds up from having no one to talk to. It quite literally keeps me alive...";
-const contactContent = "Hey there, if you'd like to contact me, feel free to send me an email";
-//array to store user posts
-let posts = [];
-
+const mongoose = require("mongoose");
 
 const app = express();
-
 app.set('view engine', 'ejs');
 ////////////////////////////////
 // EXPRESS' built-in body parser
@@ -21,17 +12,40 @@ app.use(express.json());
 app.use(express.urlencoded({
   extended: true
 }));
+
 app.use(express.static("public"));
+
+//connecting to our MongoDB Atlas cluster...
+mongoose.connect("mongodb://0.0.0.0:27017/journalDB");
+
+
+/// Consts & Var declarations
+const homeStartingContent = "A journal of a Lonely 4 that can cook.";
+const aboutContent = "I'm just a guy trying to find himself in life, struggling through but somehow managing to survive in this cold world.I find that writing about life, the things I'm dealing with and feeling, helps cope with them. It relieves some of the pressure that builds up from having no one to talk to. It quite literally keeps me alive...";
+const contactContent = "Hey there, if you'd like to contact me, feel free to send me an email";
+
+//Mongoose Schemas & Models
+const postSchema = mongoose.Schema({
+  title: String,
+  content: String
+});
+
+const Post = new mongoose.model("Post", postSchema);
 
 
 //////// Get methods for individiual pages:
-
-app.get("/", function(req, res) {
-  res.render("home", {
-    homeStartingContent,
-    posts: posts
-  });
-})
+app.get("/", function(req, res){
+Post.find({}, function(err, foundPosts){
+  if(!err){
+    res.render("home", {
+      homeStartingContent,
+      posts: foundPosts
+    });
+  } else {
+    console.log("Unable to retrieve posts.");
+  }
+});
+});
 
 app.get("/contact", function(req, res) {
   res.render("contact", {
@@ -54,27 +68,34 @@ app.get("/compose", function(req, res) {
 
 //post method to store user input when composing a post
 app.post("/compose", function(req, res) {
-  const newPost = {
-    TITLE: req.body.postTitle,
-    CONTENT: req.body.postContent
-  };
-  posts.push(newPost);
-  res.redirect("/")
+const post = new Post({
+  title: req.body.postTitle,
+  content: req.body.postContent
+});
+post.save(function(err){
+  if(!err){
+    res.redirect("/");
+  }
+});
 });
 
 //Express routing for retreiving posts & rendering a seperate page for them!
-app.get("/posts/:postName", function(req, res) {
+app.get("/posts/:postId", function(req, res) {
   //lowerCase method from the Lodash library
-  const reqTitle = _.lowerCase(req.params.postName);
-  posts.forEach(function(post) {
-    const storedTitle = _.lowerCase(post.TITLE);
-    if (reqTitle === storedTitle) {
-      res.render("post", {
-        thisPost: post});
-    }
+  const reqPostID = req.params.postId;
+  Post.findOne({_id: reqPostID}, function(err, foundPost){
+    res.render("post", {
+      title: foundPost.title,
+      content: foundPost.content
+    });
   });
 });
 
+/* let port = process.env.PORT;
+if(port == null || port == ""){
+  port = 3000;
+} */
+
 app.listen(3000, function() {
-  console.log("Server started on port 3000");
+  console.log("Server started successfully.");
 });
